37f43cd4177e23ff443f9ec34a473b45afc6389b
isaaygun <37147760+isaaygun@users.noreply.github.com>
2023-01-09 13:02:23 +0300

    lesson 02

--- app.js
+++ app.js
@@ -1,11 +1,20 @@
-const express=require('express');
-const app=express();
+const express = require('express');
+const app = express();
 // const app = require('express')();
 
 const port = 3000;
 
+app.set('view engine', 'ejs'); //template engine
+
+//MIDDLEWARE(static)
+app.use(express.static('public')); //static dosyalar public klasörü içindedir
+
+//ROUTES
 app.get('/', (req, res) => {
-  res.status(200).send('merhaba index sayfası');
+  res.render('index', { page_name : 'index' });
+});
+app.get('/about', (req, res) => {
+  res.render('about', { page_name : 'about' });
 });
 
 app.listen(port, () => {
